7e3e5b30d671c83da8434ffba8e033975cf9f048
leoniehouzer <ge84maf@tum.de>
2026-07-08 15:14:04 +0200

    meet (should be done)

--- src/cdomains/apron/subpoly/subPolyhedraCore.apron.ml
+++ src/cdomains/apron/subpoly/subPolyhedraCore.apron.ml
@@ -321,6 +321,7 @@ module SubPoly (Var : Var) (I : IntervalSig) = struct
     let new_affeq = Matrix.linear_disjunct new_a.affeq new_b.affeq in
     {affeq = new_affeq; intervals = new_intervals; infos = new_a.infos}
 
+    
   (** [meet a b] returns a subpolyhedra resulting from the meet of two subpolyhedras a and b.
       We assume that the info fields of slack variables are canonical. 
       Slack variables with an interval bound but no info field are discarded, as they cannot be matched
